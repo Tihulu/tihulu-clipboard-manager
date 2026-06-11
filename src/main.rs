f7ea8658e9f1bf2de0729fbec2df8ca04bd724d5
Cagil Benibol <40742160+Tihulu@users.noreply.github.com>
2026-06-12 01:30:39 +0200

    Add applet entrypoint

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+mod app;
+mod config;
+mod i18n;
+mod model;
+mod storage;
+
+fn main() -> cosmic::iced::Result {
+    let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
+    i18n::init(&requested_languages);
+
+    cosmic::applet::run::<app::AppModel>(())
+}
