f423d846c360d886d26c75122029e64e1a3e952f
Cagil Benibol <40742160+Tihulu@users.noreply.github.com>
2026-06-12 08:32:53 +0200

    Verify xkbcommon in CI dependencies

--- scripts/ci-deps.sh
+++ scripts/ci-deps.sh
@@ -4,4 +4,14 @@
 set -Eeuo pipefail
 
 sudo apt-get update
-sudo apt-get install -y build-essential pkg-config libssl-dev libwayland-dev libxkbcommon-dev libegl1-mesa-dev
+sudo apt-get install -y \
+  build-essential \
+  pkg-config \
+  libssl-dev \
+  libwayland-dev \
+  libxkbcommon-dev \
+  libxkbcommon-x11-dev \
+  libegl1-mesa-dev
+
+pkg-config --exists xkbcommon
+pkg-config --modversion xkbcommon
