017f67bf641313b01c9be774690c102c9bedf239
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-26 04:09:17 +0200

    Add hardened encryption and Safe Core config

--- src/config.rs
+++ src/config.rs
@@ -2,8 +2,13 @@
 
 use cosmic::cosmic_config::{self, CosmicConfigEntry, cosmic_config_derive::CosmicConfigEntry};
 
+pub const SAFE_CORE_MAX_ENTRIES: usize = 50;
+pub const SAFE_CORE_MAX_AGE_DAYS: u64 = 7;
+pub const SAFE_CORE_MAX_TEXT_BYTES: usize = 64 * 1024;
+pub const SAFE_CORE_MAX_IMAGE_BYTES: usize = 5 * 1024 * 1024;
+
 #[derive(Debug, Clone, CosmicConfigEntry, Eq, PartialEq)]
-#[version = 4]
+#[version = 5]
 pub struct Config {
     pub confirm_before_clear_all: bool,
     pub max_entries: usize,
@@ -17,6 +22,7 @@ pub struct Config {
     pub image_clipboard: bool,
     pub limit_image_size: bool,
     pub max_image_bytes: usize,
+    pub safe_core: bool,
 }
 
 impl Default for Config {
@@ -34,6 +40,76 @@ impl Default for Config {
             image_clipboard: true,
             limit_image_size: true,
             max_image_bytes: 25 * 1024 * 1024,
+            safe_core: false,
         }
+    }
+}
+
+impl Config {
+    pub fn hardened(mut self) -> Self {
+        self.encrypt_history = true;
+
+        if self.safe_core {
+            self.limit_image_size = true;
+            self.image_clipboard = false;
+            self.max_entries = self.max_entries.min(SAFE_CORE_MAX_ENTRIES);
+            self.max_age_days = if self.max_age_days == 0 {
+                SAFE_CORE_MAX_AGE_DAYS
+            } else {
+                self.max_age_days.min(SAFE_CORE_MAX_AGE_DAYS)
+            };
+            self.max_text_bytes = self.max_text_bytes.min(SAFE_CORE_MAX_TEXT_BYTES);
+            self.max_image_bytes = self.max_image_bytes.min(SAFE_CORE_MAX_IMAGE_BYTES);
+        }
+
+        self
+    }
+
+    pub fn effective_image_clipboard(&self) -> bool {
+        self.image_clipboard && !self.safe_core
+    }
+
+    pub fn effective_limit_image_size(&self) -> bool {
+        self.limit_image_size || self.safe_core
+    }
+
+    pub fn effective_max_entries(&self) -> usize {
+        if self.safe_core {
+            self.max_entries.min(SAFE_CORE_MAX_ENTRIES)
+        } else {
+            self.max_entries
+        }
+    }
+
+    pub fn effective_max_age_days(&self) -> u64 {
+        if self.safe_core {
+            if self.max_age_days == 0 {
+                SAFE_CORE_MAX_AGE_DAYS
+            } else {
+                self.max_age_days.min(SAFE_CORE_MAX_AGE_DAYS)
+            }
+        } else {
+            self.max_age_days
+        }
+    }
+
+    pub fn effective_max_text_bytes(&self) -> usize {
+        if self.safe_core {
+            self.max_text_bytes.min(SAFE_CORE_MAX_TEXT_BYTES)
+        } else {
+            self.max_text_bytes
+        }
+    }
+
+    pub fn effective_max_image_bytes(&self) -> usize {
+        if self.safe_core {
+            self.max_image_bytes.min(SAFE_CORE_MAX_IMAGE_BYTES)
+        } else {
+            self.max_image_bytes
+        }
+    }
+
+    pub fn image_previews_enabled(&self) -> bool {
+        !self.safe_core
     }
 }
