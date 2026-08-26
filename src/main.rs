a5eeb362a099a3c9eafb3e4b434df7b638c2c340
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-26 07:52:04 +0200

    Allow multiple panel applet processes

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,9 @@ mod config;
 mod i18n;
 mod model;
 mod sensitive;
-mod single_instance;
 mod storage;
 
 fn main() -> cosmic::iced::Result {
-    let _single_instance_guard = match single_instance::SingleInstanceGuard::acquire() {
-        Ok(Some(guard)) => guard,
-        Ok(None) => return Ok(()),
-        Err(error) => {
-            eprintln!("single instance lock warning: {error}");
-            let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
-            i18n::init(&requested_languages);
-            return cosmic::applet::run::<app::AppModel>(());
-        }
-    };
-
     let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
     i18n::init(&requested_languages);
 
